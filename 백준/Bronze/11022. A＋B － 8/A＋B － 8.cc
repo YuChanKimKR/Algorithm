872b05062401c7e0049b9a7a4ad6aa67cdeeb8d4
Yuchan Kim <163967232+YuChanKimKR@users.noreply.github.com>
2024-03-20 10:40:51 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int a=0, b=0, T;
+    scanf("%d", &T);
+
+    for(int i=0; i<T; i++){
+        scanf("%d %d\n", &a, &b);
+        printf("Case #%d: %d + %d = %d\n", i+1, a, b, a+b);
+    }
+
+    return 0;
+}
