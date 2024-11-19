04d5e8d64442d2b82da687035cb0e1556358453d
Yuchan Kim <163967232+YuChanKimKR@users.noreply.github.com>
2024-11-19 15:04:53 +0900

    [Bronze II] Title: 블랙잭, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,23 +1,12 @@
-# 카드의 개수 N, 목표 합 M 입력 받기
 N, M = map(int, input().split())
+numbers = list(map(int, input().split()))
+answer = 0
 
-# 카드에 쓰여 있는 수 입력 받기
-cards = list(map(int, input().split()))
-
-# 가장 가까운 합 초기화
-closest_sum = 0
-
-# 카드들을 정렬
-cards.sort()
-
-# 가능한 카드 조합을 확인하여 가장 가까운 합 찾기
 for i in range(N):
     for j in range(i + 1, N):
         for k in range(j + 1, N):
-            card_sum = cards[i] + cards[j] + cards[k]
-            # 현재 합이 목표 합 M보다 작고, 기존의 가장 가까운 합보다 크다면 갱신
-            if card_sum <= M and card_sum > closest_sum:
-                closest_sum = card_sum
+            temp_sum = numbers[i] + numbers[j] + numbers[k]
+            if temp_sum <= M and temp_sum > answer:
+                answer = temp_sum
 
-# 결과 출력
-print(closest_sum)
+print(answer)
