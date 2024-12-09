68e582c2368d1ea8704431b002b0675305477dc5
Yuchan Kim <163967232+YuChanKimKR@users.noreply.github.com>
2024-12-09 20:19:13 +0900

    [Silver III] Title: 통계학, Time: 308 ms, Memory: 143024 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,20 +1,33 @@
+import collections
 import sys
 input = sys.stdin.readline
 
-n = int(input().strip())
-nums = [int(input().strip()) for _ in range(n)]
+N = int(input())
+arr = list()
 
-total = sum(nums)
-print(round(total / n))
+for _ in range(N):
+    nums = int(input())
+    arr.append(nums)
 
-nums.sort()
-print(nums[n // 2])
+arr.sort()
 
-from collections import Counter
-cnt = Counter(nums).most_common()
-if len(cnt) > 1 and cnt[0][1] == cnt[1][1]:
-    print(cnt[1][0])
-else:
-    print(cnt[0][0])
+first = round(sum(arr) / N)
+second = arr[(len(arr) // 2)]
 
-print(nums[-1] - nums[0])
+d = collections.Counter(arr);
+M = max(d.values())
+m = 0
+for i in range(N):
+    if (i==0)|(arr[i-1]!=arr[i]):k=1
+    else:k+=1
+    if M==k:
+        if m:m=arr[i];break
+        else:m=arr[i]
+            
+for i in arr:
+    four = max(arr[0], i) - min(arr[0], i)
+    
+print(first)
+print(second)
+print(m)
+print(four)
